19c2e091aa2ca970f3975fb125ce60db47cb5b18
Helmut Januschka <helmut@januschka.com>
2015-05-06 19:06:02 +0200

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Helmut on 06.05.15.
+//  Copyright (c) 2015 Helmut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by Helmut on 06.05.15.
+//  Copyright (c) 2015 Helmut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
